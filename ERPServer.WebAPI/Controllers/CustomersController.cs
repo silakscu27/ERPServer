a592c55964ecfe64c27a4a34f659c590f4016861
Sila Kuscu <silaksc27@gmail.com>
2024-11-12 19:05:32 +0300

    [Create] Customer Controller created

---
+++ ERPServer.WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using ERPServer.Application.Features.Customers.CreateCustomer;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class CustomersController : ApiController
+{
+    public CustomersController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
