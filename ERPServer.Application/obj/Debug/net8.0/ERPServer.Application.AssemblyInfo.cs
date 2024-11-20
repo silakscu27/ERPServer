e9f59692e62667dfee627a9cbdabf1701510851b
Sila Kuscu <silaksc27@gmail.com>
2024-11-20 14:58:54 +0300

    [rename] product to products

--- ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
+++ ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ab0369c1ea4b3c50a0e85a060cbde6b48a014c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
