24af07a31906a8662d4bb1ebaecec8cac6a22f49
Sila Kuscu <silaksc27@gmail.com>
2024-11-20 14:57:30 +0300

    [Update] updated database

--- ERPServer.Infrastructure/obj/Debug/net8.0/ERPServer.Infrastructure.AssemblyInfo.cs
+++ ERPServer.Infrastructure/obj/Debug/net8.0/ERPServer.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32abbdcf848e19c3f39c177c1fd915689029ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
+++ ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ab0369c1ea4b3c50a0e85a060cbde6b48a014c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32abbdcf848e19c3f39c177c1fd915689029ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
