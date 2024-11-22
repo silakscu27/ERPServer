e8756442ba05fbb68ea2812ad73c8bc85754ed92
Sila Kuscu <silaksc27@gmail.com>
2024-11-22 16:04:37 +0300

    [Update] Updated database

--- ERPServer.Infrastructure/obj/Debug/net8.0/ERPServer.Infrastructure.AssemblyInfo.cs
+++ ERPServer.Infrastructure/obj/Debug/net8.0/ERPServer.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c283a4b41b6ee6bee8ae04d06169b7a8ea236139")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
