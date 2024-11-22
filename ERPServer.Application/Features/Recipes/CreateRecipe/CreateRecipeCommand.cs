4b5cf397e658c9bb691f3d3436da8027f65c9cb7
Sila Kuscu <silaksc27@gmail.com>
2024-11-22 16:34:32 +0300

    [Update] Updated create recipe commands

--- ERPServer.Application/Features/Recipes/CreateRecipe/CreateRecipeCommand.cs
+++ ERPServer.Application/Features/Recipes/CreateRecipe/CreateRecipeCommand.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using ERPServer.Domain.Dtos;
+using MediatR;
+using TS.Result;
 
-namespace ERPServer.Application.Features.Recipes.CreateRecipe
-{
-    internal class CreateRecipeCommand
-    {
-    }
-}
+namespace ERPServer.Application.Features.Recipes.CreateRecipe;
+public sealed record CreateRecipeCommand(
+    Guid ProductId,
+    List<RecipeDetailDto> Details) : IRequest<Result<string>>;
--- ERPServer.Application/Features/Recipes/CreateRecipe/CreateRecipeCommandHandler.cs
+++ ERPServer.Application/Features/Recipes/CreateRecipe/CreateRecipeCommandHandler.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
 
-namespace ERPServer.Application.Features.Recipes.CreateRecipe
+namespace ERPServer.Application.Features.Recipes.CreateRecipe;
+
+internal sealed class CreateRecipeCommandHandler(
+    IRecipeRepository recipeRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<CreateRecipeCommand, Result<string>>
 {
-    internal class CreateRecipeCommandHandler
+    public async Task<Result<string>> Handle(CreateRecipeCommand request, CancellationToken cancellationToken)
     {
+        bool isRecipeExists = await recipeRepository.AnyAsync(p => p.ProductId == request.ProductId, cancellationToken);
+
+        if (isRecipeExists)
+        {
+            return Result<string>.Failure("Bu ürüne ait reçete daha önce oluşturulmuş");
+        }
+
+        Recipe recipe = new()
+        {
+            ProductId = request.ProductId,
+            Details = request.Details.Select(s =>
+                new RecipeDetail()
+                {
+                    ProductId = s.ProductId,
+                    Quantity = s.Quantity
+                }).ToList()
+        };
+
+        await recipeRepository.AddAsync(recipe);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Reçete kaydı başarıyla tamamlandı";
     }
 }
