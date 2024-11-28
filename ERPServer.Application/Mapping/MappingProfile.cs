3a1675ae646d45af3fb51133bac89689853415bc
Sila Kuscu <silaksc27@gmail.com>
2024-11-28 14:50:40 +0300

    [Add]  Added order create and update mapped

--- ERPServer.Application/Mapping/MappingProfile.cs
+++ ERPServer.Application/Mapping/MappingProfile.cs
@@ -3,6 +3,8 @@ using ERPServer.Application.Features.Customers.CreateCustomer;
 using ERPServer.Application.Features.Customers.UpdateCustomer;
 using ERPServer.Application.Features.Depots.CreateDepot;
 using ERPServer.Application.Features.Depots.UpdateDepot;
+using ERPServer.Application.Features.Orders.CreateOrder;
+using ERPServer.Application.Features.Orders.UpdateOrder;
 using ERPServer.Domain.Entities;
 namespace ERPServer.Application.Mapping
 {
@@ -15,6 +17,21 @@ namespace ERPServer.Application.Mapping
 
             CreateMap<CreateDepotCommand, Depot>();
             CreateMap<UpdateDepotCommand, Depot>();
+
+            CreateMap<CreateOrderCommand, Order>()
+            .ForMember(member => member.Details,
+            options =>
+            options.MapFrom(p => p.Details.Select(s => new OrderDetail
+            {
+                Price = s.Price,
+                ProductId = s.ProductId,
+                Quantity = s.Quantity
+            }).ToList()));
+
+            CreateMap<UpdateOrderCommand, Order>()
+                .ForMember(member =>
+                member.Details,
+                options => options.Ignore());
         }
     }
 }
