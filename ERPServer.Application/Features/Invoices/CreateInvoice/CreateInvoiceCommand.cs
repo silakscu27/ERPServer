5474c8010f796b3a50ad7941992655d907fdfe77
Sila Kuscu <silaksc27@gmail.com>
2024-12-05 23:12:14 +0300

    [Create] Created invoice create command

---
+++ ERPServer.Application/Features/Invoices/CreateInvoice/CreateInvoiceCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPServer.Application.Features.Invoices.CreateInvoice
+{
+    internal class CreateInvoiceCommand
+    {
+    }
+}
