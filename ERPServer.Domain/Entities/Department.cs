463ddc38eed61fc3dd3ec7dd6c87968e2565f1f0
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 15:29:27 +0300

    [Create] Created Department entity

---
+++ ERPServer.Domain/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Department : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
