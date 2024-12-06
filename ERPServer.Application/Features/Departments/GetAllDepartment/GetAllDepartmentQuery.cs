8c7ea3ad921297ee633a867bc833668df714f35e
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 16:24:22 +0300

    [Create] Department Get All query created

---
+++ ERPServer.Application/Features/Departments/GetAllDepartment/GetAllDepartmentQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Departments.GetAllDepartment;
+
+public sealed record GetAllDepartmentQuery() : IRequest<Result<List<Department>>>;
---
+++ ERPServer.Application/Features/Departments/GetAllDepartment/GetAllDepartmentQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Departments.GetAllDepartment;
+
+internal sealed class GetAllDepartmentQueryHandler(
+    IDepartmentRepository departmentRepository) : IRequestHandler<GetAllDepartmentQuery, Result<List<Department>>>
+{
+    public async Task<Result<List<Department>>> Handle(GetAllDepartmentQuery request, CancellationToken cancellationToken)
+    {
+        // Fetch all departments and order them by name
+        List<Department> departments = await departmentRepository.GetAll()
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+
+        return departments;
+    }
+}
