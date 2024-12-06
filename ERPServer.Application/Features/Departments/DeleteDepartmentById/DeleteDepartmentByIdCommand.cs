6f97785f46e2fbd9be595b6ee517a2e453c5b5cb
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 16:09:28 +0300

    [Create] Department Delete by id command created

---
+++ ERPServer.Application/Features/Departments/DeleteDepartmentById/DeleteDepartmentByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Departments.DeleteDepartmentById;
+
+public sealed record DeleteDepartmentByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Departments/DeleteDepartmentById/DeleteDepartmentByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Departments.DeleteDepartmentById;
+
+internal sealed class DeleteDepartmentByIdCommandHandler(
+    IDepartmentRepository departmentRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteDepartmentByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteDepartmentByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve the department by its ID
+        Department department = await departmentRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (department is null)
+        {
+            return Result<string>.Failure("Departman bulunamadı");
+        }
+
+        // Delete the department
+        departmentRepository.Delete(department);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Departman başarıyla silindi";
+    }
+}
