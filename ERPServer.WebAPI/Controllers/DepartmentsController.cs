44e5976bf35e996b160cbe7b50dfc3792403dc00
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 17:07:01 +0300

    [Create] Created Department Controller

---
+++ ERPServer.WebAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.Departments.CreateDepartment;
+using ERPServer.Application.Features.Departments.DeleteDepartmentById;
+using ERPServer.Application.Features.Departments.GetAllDepartment;
+using ERPServer.Application.Features.Departments.UpdateDepartment;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class DepartmentsController : ApiController
+{
+    public DepartmentsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllDepartmentQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateDepartmentCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteDepartmentByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateDepartmentCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
