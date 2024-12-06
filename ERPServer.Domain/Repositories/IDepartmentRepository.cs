02beea039a5ab74eda81efeb593505f2f5fa3888
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 15:30:11 +0300

    [Create] Created Department interface repo

--- ERPServer.Domain/Repositories/IDepartmentRepository.cs
+++ ERPServer.Domain/Repositories/IDepartmentRepository.cs
@@ -2,6 +2,6 @@
 using GenericRepository;
 
 namespace ERPServer.Domain.Repositories;
-public interface IDepotRepository : IRepository<Depot>
+public interface IDepartmentRepository : IRepository<Department>
 {
 }
