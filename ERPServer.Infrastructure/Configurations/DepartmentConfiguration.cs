d56f5d3be2b10fc3cac8e10bcc9f1419db849f1b
Sila Kuscu <silaksc27@gmail.com>
2024-12-06 15:35:10 +0300

    [Create] Created Department Configuration

---
+++ ERPServer.Infrastructure/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    internal sealed class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.Description).HasColumnType("varchar(250)");
+        }
+    }
+}
