52337e9680ceecac20638057220b8e381573e3cf
Sila Kuscu <silaksc27@gmail.com>
2024-12-07 18:16:15 +0300

    [Add] Added navigation feature to Department class

--- ERPServer.Domain/Entities/Department.cs
+++ ERPServer.Domain/Entities/Department.cs
@@ -7,4 +7,6 @@ public sealed class Department : Entity
     public new Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
+
+    public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
 }
