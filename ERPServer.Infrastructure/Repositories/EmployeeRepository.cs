813a0e7b54817b390920901437972136aa067156
Sila Kuscu <silaksc27@gmail.com>
2024-12-07 18:18:25 +0300

    [Create] Created Employee repository class

---
+++ ERPServer.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class EmployeeRepository : Repository<Employee, ApplicationDbContext>, IEmployeeRepository
+{
+    public EmployeeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
