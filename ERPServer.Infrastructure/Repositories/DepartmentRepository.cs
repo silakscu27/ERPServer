80f1e014f5324f64704d391c056dd5ab9fc290eb
Sila Kuscu <silaksc27@gmail.com>
2024-12-08 23:39:43 +0300

    [Add] Added GetById method

--- ERPServer.Infrastructure/Repositories/DepartmentRepository.cs
+++ ERPServer.Infrastructure/Repositories/DepartmentRepository.cs
@@ -2,11 +2,22 @@
 using ERPServer.Domain.Repositories;
 using ERPServer.Infrastructure.Context;
 using GenericRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace ERPServer.Infrastructure.Repositories;
+
 internal sealed class DepartmentRepository : Repository<Department, ApplicationDbContext>, IDepartmentRepository
 {
+    private readonly ApplicationDbContext _dbContext;
+
     public DepartmentRepository(ApplicationDbContext context) : base(context)
     {
+        _dbContext = context; 
+    }
+
+    public async Task<Department?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Departments
+            .FirstOrDefaultAsync(d => d.Id == id, cancellationToken);
     }
 }
