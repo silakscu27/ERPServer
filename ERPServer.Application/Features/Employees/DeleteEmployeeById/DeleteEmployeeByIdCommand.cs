1169ee2fde29b61c7c0da1067b6133aab363b4c7
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 14:32:33 +0300

    [Create] Created employee delete by id command

---
+++ ERPServer.Application/Features/Employees/DeleteEmployeeById/DeleteEmployeeByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Employees.DeleteEmployeeById;
+
+public sealed record DeleteEmployeeByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Employees/DeleteEmployeeById/DeleteEmployeeByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Employees.DeleteEmployeeById;
+
+internal sealed class DeleteEmployeeByIdCommandHandler(
+    IEmployeeRepository employeeRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteEmployeeByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteEmployeeByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve the employee by its ID
+        Employee employee = await employeeRepository.GetByExpressionAsync(e => e.Id == request.Id, cancellationToken);
+
+        if (employee is null)
+        {
+            return Result<string>.Failure("Çalışan bulunamadı");
+        }
+
+        // Delete the employee
+        employeeRepository.Delete(employee);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Çalışan başarıyla silindi";
+    }
+}
