1c8a971aeee255514c2a55ed43f02a9ab5d40130
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 15:06:02 +0300

    [Create] Created Employees Controller

---
+++ ERPServer.WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.Employees.CreateEmployee;
+using ERPServer.Application.Features.Employees.DeleteEmployeeById;
+using ERPServer.Application.Features.Employees.GetAllEmployee;
+using ERPServer.Application.Features.Employees.UpdateEmployee;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class EmployeesController : ApiController
+{
+    public EmployeesController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllEmployeeQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteEmployeeByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
