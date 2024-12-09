2a2d04a98e15922cd0fb9bd029ec152aecd31fd9
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 16:35:54 +0300

    [Create] Created Order entity

---
+++ ERPServer.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Order : Entity
+{
+    public new Guid Id { get; set; } 
+
+    public Guid CustomerId { get; set; } 
+
+    public DateTime OrderDate { get; set; } 
+
+    public decimal TotalAmount { get; set; } 
+
+    public string Status { get; set; } = string.Empty; 
+
+    public Customer Customer { get; set; } = null!; 
+}
