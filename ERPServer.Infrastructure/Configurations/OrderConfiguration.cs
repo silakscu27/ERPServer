cccc497307063fdc8456894f2c94181e0fa8567d
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 17:13:53 +0300

    [Create] Created Order Configuration

---
+++ ERPServer.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    public sealed class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.OrderDate)
+                .IsRequired()
+                .HasColumnType("datetime"); 
+
+            builder.Property(o => o.TotalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)"); 
+
+            builder.Property(o => o.Status)
+                .IsRequired()
+                .HasMaxLength(50); 
+
+            builder.HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+        }
+    }
+}
