23f6274abcaf1a598d73ce7c68f68ee3a14ff6bf
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 15:23:43 +0300

    [Update]  Some new parameters added

--- ERPServer.Domain/Entities/Customer.cs
+++ ERPServer.Domain/Entities/Customer.cs
@@ -1,13 +1,16 @@
 ﻿using ERPServer.Domain.Abstractions;
 
 namespace ERPServer.Domain.Entities;
+
 public sealed class Customer : Entity
 {
-    public string Name { get; set; } = string.Empty;
-    public string TaxDepartment { get; set; } = string.Empty;
-    public string TaxNumber { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
-    public string Town { get; set; } = string.Empty;
-    public string FullAddress { get; set; } = string.Empty;
-
+    public string FirstName { get; set; } = string.Empty; 
+    public string LastName { get; set; } = string.Empty; 
+    public string PhoneNumber { get; set; } = string.Empty; 
+    public string Email { get; set; } = string.Empty; 
+    public string TaxDepartment { get; set; } = string.Empty; 
+    public string TaxNumber { get; set; } = string.Empty; 
+    public string City { get; set; } = string.Empty; 
+    public string Town { get; set; } = string.Empty; 
+    public string FullAddress { get; set; } = string.Empty; 
 }
