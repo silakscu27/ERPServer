7da20cc0bfe6d71258406140d9a121596ba8b5b3
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 15:31:26 +0300

    [Update] Configuration of newly added parameters was done

--- ERPServer.Infrastructure/Configurations/CustomerConfiguration.cs
+++ ERPServer.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -8,7 +8,33 @@ namespace ERPServer.Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
-            builder.Property(p => p.TaxNumber).HasColumnType("varchar(11)");
+            builder.Property(p => p.TaxNumber)
+                .HasColumnType("varchar(11)");
+
+            builder.Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.City)
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Town)
+                .HasMaxLength(50);
+
+            builder.Property(p => p.FullAddress)
+                .HasMaxLength(150);
         }
     }
 }
