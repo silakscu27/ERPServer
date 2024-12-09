a8ca826ee0738a73388a6c31ff52daab68d9e00a
Sila Kuscu <silaksc27@gmail.com>
2024-12-09 14:49:23 +0300

    [Create] Created employee get all query

---
+++ ERPServer.Application/Features/Employees/GetAllEmployee/GetAllEmployeeQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Employees.GetAllEmployee;
+
+public sealed record GetAllEmployeeQuery() : IRequest<Result<List<Employee>>>;
---
+++ ERPServer.Application/Features/Employees/GetAllEmployee/GetAllEmployeeQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Employees.GetAllEmployee;
+
+internal sealed class GetAllEmployeeQueryHandler(
+    IEmployeeRepository employeeRepository) : IRequestHandler<GetAllEmployeeQuery, Result<List<Employee>>>
+{
+    public async Task<Result<List<Employee>>> Handle(GetAllEmployeeQuery request, CancellationToken cancellationToken)
+    {
+        // Fetch all employees and order them by last name
+        List<Employee> employees = await employeeRepository.GetAll()
+            .OrderBy(e => e.LastName)
+            .ToListAsync(cancellationToken);
+
+        return employees;
+    }
+}
