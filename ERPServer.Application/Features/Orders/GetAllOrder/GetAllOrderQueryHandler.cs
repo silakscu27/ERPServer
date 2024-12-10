4bbf45809627e3b531a3e79306fd51bf93c164cd
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:14:39 +0300

    [Create] Created Order get all query

---
+++ ERPServer.Application/Features/Orders/GetAllOrder/GetAllOrderQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Orders.GetAllOrder;
+
+internal sealed class GetAllOrderQueryHandler(
+    IOrderRepository orderRepository) : IRequestHandler<GetAllOrderQuery, Result<List<Order>>>
+{
+    public async Task<Result<List<Order>>> Handle(GetAllOrderQuery request, CancellationToken cancellationToken)
+    {
+        // Retrieve all orders and sort by order date
+        List<Order> orders = await orderRepository.GetAll()
+            .OrderBy(o => o.OrderDate)
+            .ToListAsync(cancellationToken);
+
+        return Result<List<Order>>.Succeed(orders);
+    }
+}
