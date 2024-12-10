2e163593281acd27ae3463699b55f5fac4bdc623
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:31:55 +0300

    [Create] Created Order Controller

---
+++ ERPServer.WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using ERPServer.Application.Features.Orders.CreateOrder;
+using ERPServer.Application.Features.Orders.DeleteOrderById;
+using ERPServer.Application.Features.Orders.GetAllOrder;
+using ERPServer.Application.Features.Orders.UpdateOrder;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers
+{
+    public sealed class OrdersController : ApiController
+    {
+        public OrdersController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(GetAllOrderQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var request = new DeleteOrderByIdCommand(id);
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
