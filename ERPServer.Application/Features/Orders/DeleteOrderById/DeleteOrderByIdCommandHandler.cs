05862a9ea393a5c3fe0103ffa9ffb17aa02ce579
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:14:18 +0300

    [Create] Created Order get by id command

---
+++ ERPServer.Application/Features/Orders/DeleteOrderById/DeleteOrderByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Orders.DeleteOrderById;
+
+internal sealed class DeleteOrderByIdCommandHandler(
+    IOrderRepository orderRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteOrderByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteOrderByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve order by ID
+        Order order = await orderRepository.GetByExpressionAsync(o => o.Id == request.Id, cancellationToken);
+
+        if (order is null)
+        {
+            return Result<string>.Failure("Sipariş bulunamadı");
+        }
+
+        // Delete order
+        orderRepository.Delete(order);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result<string>.Succeed("Sipariş başarıyla silindi");
+    }
+}
