bb6003e178e0627a7d371b780b5d05fc8f651f99
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:17:52 +0300

    [Add] A method was defined to get customer IDs

--- ERPServer.Domain/Repositories/ICustomerRepository.cs
+++ ERPServer.Domain/Repositories/ICustomerRepository.cs
@@ -4,4 +4,5 @@ using GenericRepository;
 namespace ERPServer.Domain.Repositories;
 public interface ICustomerRepository : IRepository<Customer>
 {
+    Task<List<Customer>> GetByIdsAsync(List<Guid> customerIds, CancellationToken cancellationToken);
 }
