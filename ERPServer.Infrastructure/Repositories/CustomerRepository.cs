b40d229586070c9a8d36cdaa9968f6ae6145bfd2
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:25:27 +0300

    [Add] Implement the missing GetByIdsAsync method

--- ERPServer.Infrastructure/Repositories/CustomerRepository.cs
+++ ERPServer.Infrastructure/Repositories/CustomerRepository.cs
@@ -2,11 +2,23 @@
 using ERPServer.Domain.Repositories;
 using ERPServer.Infrastructure.Context;
 using GenericRepository;
+using Microsoft.EntityFrameworkCore;
 
-namespace ERPServer.Infrastructure.Repositories;
-internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
+namespace ERPServer.Infrastructure.Repositories
 {
-    public CustomerRepository(ApplicationDbContext context) : base(context)
+    internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CustomerRepository(ApplicationDbContext context) : base(context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<List<Customer>> GetByIdsAsync(List<Guid> customerIds, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Customers
+                .Where(c => customerIds.Contains(c.Id))
+                .ToListAsync(cancellationToken);
+        }
     }
 }
