474247f8d24baa09cc0312879b48af2174fb78c6
Sila Kuscu <silaksc27@gmail.com>
2024-12-10 14:19:06 +0300

    [Add] Added mapping for CreateOrderCommand and UpdateOrderCommand

--- ERPServer.Application/Mapping/MappingProfile.cs
+++ ERPServer.Application/Mapping/MappingProfile.cs
@@ -5,21 +5,31 @@ using ERPServer.Application.Features.Departments.CreateDepartment;
 using ERPServer.Application.Features.Departments.UpdateDepartment;
 using ERPServer.Application.Features.Employees.CreateEmployee;
 using ERPServer.Application.Features.Employees.UpdateEmployee;
+using ERPServer.Application.Features.Orders.CreateOrder;
+using ERPServer.Application.Features.Orders.UpdateOrder;
 using ERPServer.Domain.Entities;
+
 namespace ERPServer.Application.Mapping
 {
     public sealed class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            // Customer mappings
             CreateMap<CreateCustomerCommand, Customer>();
             CreateMap<UpdateCustomerCommand, Customer>();
 
+            // Department mappings
             CreateMap<CreateDepartmentCommand, Department>();
             CreateMap<UpdateDepartmentCommand, Department>();
 
+            // Employee mappings
             CreateMap<CreateEmployeeCommand, Employee>();
             CreateMap<UpdateEmployeeCommand, Employee>();
+
+            // Order mappings
+            CreateMap<CreateOrderCommand, Order>();
+            CreateMap<UpdateOrderCommand, Order>();
         }
     }
 }
