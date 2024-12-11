7d1b53df2b6fa694744a275fcb0100818f8a9007
Sila Kuscu <silaksc27@gmail.com>
2024-12-11 19:02:34 +0300

    [Create] Created Stock entity

---
+++ ERPServer.Domain/Entities/Stock.cs
@@ -0,0 +1,14 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Stock : Entity
+{
+    public new Guid Id { get; set; }
+    public string ProductName { get; set; } = string.Empty; 
+    public Guid CategoryId { get; set; } 
+    public int Quantity { get; set; } 
+    public decimal Price { get; set; }
+    public DateTime LastUpdatedDate { get; set; } 
+
+    public Category Category { get; set; } = null!; 
