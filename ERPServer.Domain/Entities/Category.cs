ef825e4ebf76e825d615a1ab309619f1645e51c2
Sila Kuscu <silaksc27@gmail.com>
2024-12-11 19:02:26 +0300

    [Create] Created Category entity

---
+++ ERPServer.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Category : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; } 
+
+    public ICollection<Stock> Stocks { get; set; } = new HashSet<Stock>();
+}
