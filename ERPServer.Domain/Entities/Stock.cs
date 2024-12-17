5685345178bc99cc8d4f85538f8744ccc39a031a
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 14:14:57 +0300

    [Add] Added new properties

--- ERPServer.Domain/Entities/Stock.cs
+++ ERPServer.Domain/Entities/Stock.cs
@@ -5,10 +5,11 @@ namespace ERPServer.Domain.Entities;
 public sealed class Stock : Entity
 {
     public new Guid Id { get; set; }
-    public string ProductName { get; set; } = string.Empty; 
-    public Guid CategoryId { get; set; } 
-    public int Quantity { get; set; } 
+    public string ProductName { get; set; } = string.Empty;
+    public Guid CategoryId { get; set; }
+    public int Quantity { get; set; }
     public decimal Price { get; set; }
-    public DateTime LastUpdatedDate { get; set; } 
+    public DateTime LastUpdatedDate { get; set; }
 
-    public Category Category { get; set; } = null!; 
+    public Category Category { get; set; } = null!;
+}
