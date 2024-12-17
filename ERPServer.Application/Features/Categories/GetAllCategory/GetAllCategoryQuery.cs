9e3cfa28a5a65489f4a33538cc367a539abfa941
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 17:50:04 +0300

    [Create] Created get all category query

---
+++ ERPServer.Application/Features/Categories/GetAllCategory/GetAllCategoryQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Categories.GetAllCategory;
+
+public sealed record GetAllCategoryQuery() : IRequest<Result<List<Category>>>;
---
+++ ERPServer.Application/Features/Categories/GetAllCategory/GetAllCategoryQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Categories.GetAllCategory;
+
+internal sealed class GetAllCategoryQueryHandler(
+    ICategoryRepository categoryRepository) : IRequestHandler<GetAllCategoryQuery, Result<List<Category>>>
+{
+    public async Task<Result<List<Category>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+    {
+        List<Category> categories = await categoryRepository.GetAll()
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+
+        return categories.Any()
+            ? Result<List<Category>>.Succeed(categories)
+            : Result<List<Category>>.Failure("Kategori bulunamadı");
+    }
+}
