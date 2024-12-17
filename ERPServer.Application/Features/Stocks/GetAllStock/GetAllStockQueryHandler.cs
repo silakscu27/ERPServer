3823832b8be0374d183e5227889736088951eb01
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 15:08:32 +0300

    [Create] Created Get all stock query

---
+++ ERPServer.Application/Features/Stocks/GetAllStock/GetAllStockQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.GetAllStock;
+
+internal sealed class GetAllStockQueryHandler(
+    IStockRepository stockRepository) : IRequestHandler<GetAllStockQuery, Result<List<Stock>>>
+{
+    public async Task<Result<List<Stock>>> Handle(GetAllStockQuery request, CancellationToken cancellationToken)
+    {
+        // Retrieve all stocks, order them by ProductName, and convert to a list
+        List<Stock> stocks = await stockRepository.GetAll()
+            .OrderBy(p => p.ProductName)
+            .ToListAsync(cancellationToken);
+
+        // Return stocks if found, otherwise return a failure message
+        return stocks.Any()
+            ? Result<List<Stock>>.Succeed(stocks)
+            : Result<List<Stock>>.Failure("Stok bulunamadı");
+    }
+}
