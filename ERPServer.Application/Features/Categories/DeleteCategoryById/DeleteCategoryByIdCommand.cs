21a7a640b8ec609371b5930fbcebdb0937cc87bf
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 17:49:32 +0300

    [Create] Created delete by id category command

---
+++ ERPServer.Application/Features/Categories/DeleteCategoryById/DeleteCategoryByIdCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Categories.DeleteCategoryById;
+
+public sealed record DeleteCategoryByIdCommand(Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Categories/DeleteCategoryById/DeleteCategoryByIdCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Categories.DeleteCategoryById;
+
+internal sealed class DeleteCategoryByIdCommandHandler(
+    ICategoryRepository categoryRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteCategoryByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteCategoryByIdCommand request, CancellationToken cancellationToken)
+    {
+        Category? category = await categoryRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (category is null)
+        {
+            return Result<string>.Failure("Kategori bulunamadı");
+        }
+
+        bool hasAssociatedStocks = category.Stocks.Any();
+        if (hasAssociatedStocks)
+        {
+            return Result<string>.Failure("Bu kategoriye bağlı stoklar bulunduğu için silinemiyor");
+        }
+
+        categoryRepository.Delete(category);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result<string>.Succeed("Kategori başarıyla silindi");
+    }
+}
