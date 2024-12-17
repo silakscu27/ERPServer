88f69e3f491a084b9533f60a89b4bd2e7b5d5f11
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 14:44:48 +0300

    [Create] Created delete stock by id command

---
+++ ERPServer.Application/Features/Stocks/DeleteStockById/DeleteStockByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.DeleteStockById;
+
+public sealed record DeleteStockByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Stocks/DeleteStockById/DeleteStockByIdCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.DeleteStockById;
+
+internal sealed class DeleteStockByIdCommandHandler(
+    IStockRepository stockRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteStockByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteStockByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve the stock by its ID
+        Stock stock = await stockRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        // If the stock does not exist, return a failure result
+        if (stock is null)
+        {
+            return Result<string>.Failure("Stok bulunamadı");
+        }
+
+        // Delete the stock
+        stockRepository.Delete(stock);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        // Return a success message
+        return "Stok başarıyla silindi";
+    }
+}
