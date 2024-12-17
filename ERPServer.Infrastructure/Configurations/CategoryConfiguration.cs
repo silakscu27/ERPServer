b8329432b88ee746ae361bfb25a3fb9a8438451b
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 14:12:46 +0300

    [Create] Created Category and Stock configuration

---
+++ ERPServer.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    public sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(150); 
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(500); 
+
+            builder.HasMany(c => c.Stocks)
+                   .WithOne(s => s.Category)
+                   .HasForeignKey(s => s.CategoryId)
+                   .OnDelete(DeleteBehavior.Cascade); 
+        }
+    }
+}
---
+++ ERPServer.Infrastructure/Configurations/StockConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    public sealed class StockConfiguration : IEntityTypeConfiguration<Stock>
+    {
+        public void Configure(EntityTypeBuilder<Stock> builder)
+        {
+            builder.ToTable("Stocks");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.ProductName)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(s => s.Quantity)
+                .IsRequired();
+
+            builder.Property(s => s.Price)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(s => s.LastUpdatedDate)
+                .IsRequired();
+
+            builder.HasOne(s => s.Category)
+                   .WithMany(c => c.Stocks)
+                   .HasForeignKey(s => s.CategoryId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
