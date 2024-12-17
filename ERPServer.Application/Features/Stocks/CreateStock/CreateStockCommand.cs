bcd75f4b8088ce1bbf27ff5871e7430e1dd0d1fb
Sila Kuscu <silaksc27@gmail.com>
2024-12-17 14:28:13 +0300

    [Create] Created create stock commands

---
+++ ERPServer.Application/Features/Stocks/CreateStock/CreateStockCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.CreateStock;
+
+public sealed record CreateStockCommand(
+    string ProductName,
+    Guid CategoryId,
+    int Quantity,
+    decimal Price) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Stocks/CreateStock/CreateStockCommandHandlerValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ERPServer.Application.Features.Stocks.CreateStock;
+
+public sealed class CreateStockCommandValidator : AbstractValidator<CreateStockCommand>
+{
+    public CreateStockCommandValidator()
+    {
+        RuleFor(p => p.ProductName)
+            .NotEmpty().WithMessage("Ürün adı boş olamaz")
+            .MaximumLength(150).WithMessage("Ürün adı en fazla 150 karakter olabilir");
+
+        RuleFor(p => p.CategoryId)
+            .NotEmpty().WithMessage("Kategori ID'si boş olamaz");
+
+        RuleFor(p => p.Quantity)
+            .GreaterThanOrEqualTo(0).WithMessage("Miktar negatif olamaz");
+
+        RuleFor(p => p.Price)
+            .GreaterThan(0).WithMessage("Fiyat sıfırdan büyük olmalıdır");
+    }
+}
