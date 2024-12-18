7087e8e30dca87c3e15e0d23ce472b52d082d12d
Sila Kuscu <silaksc27@gmail.com>
2024-12-18 16:20:17 +0300

    [Create] Created Product Configuration

---
+++ ERPServer.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations;
+
+public sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Name)
+            .IsRequired() 
+            .HasMaxLength(200); 
+
+        builder.Property(p => p.Price)
+            .IsRequired() 
+            .HasColumnType("decimal(18,2)"); 
+
+        builder.HasOne(p => p.Stock)
+            .WithMany(s => s.Products)
+            .HasForeignKey(p => p.StockId)
+            .OnDelete(DeleteBehavior.Cascade); 
+    }
+}
