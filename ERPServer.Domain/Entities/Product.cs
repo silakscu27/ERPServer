0f2f9f38aee30e61918ae9cb4e4d8cf5d5cb8810
Sila Kuscu <silaksc27@gmail.com>
2024-12-18 16:09:08 +0300

    [Create] Created Product entity

---
+++ ERPServer.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Product : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public Guid StockId { get; set; }
+
+    public Stock Stock { get; set; } = null!;
+}
