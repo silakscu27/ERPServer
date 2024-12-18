d5fad83aaa286c12f36287764a378b6083b41454
Sila Kuscu <silaksc27@gmail.com>
2024-12-18 16:10:26 +0300

    [Create] Created Stock get by id query

---
+++ ERPServer.Application/Features/Stocks/GetStockById/GetStockByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.GetStockById;
+
+public sealed record GetStockByIdQuery(Guid Id) : IRequest<Result<Stock>>;
---
+++ ERPServer.Application/Features/Stocks/GetStockById/GetStockByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Stocks.GetStockById;
+
+internal sealed class GetStockByIdQueryHandler(
+    IStockRepository stockRepository) : IRequestHandler<GetStockByIdQuery, Result<Stock>>
+{
+    public async Task<Result<Stock>> Handle(GetStockByIdQuery request, CancellationToken cancellationToken)
+    {
+        Stock? stock = await stockRepository.GetAll()
+            .Include(s => s.Products) 
+            .FirstOrDefaultAsync(s => s.Id == request.Id, cancellationToken);
+
+        if (stock is null)
+        {
+            return Result<Stock>.Failure("Stok bulunamadı.");
+        }
+
+        return Result<Stock>.Succeed(stock);
+    }
+}
