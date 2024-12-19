7b2d89c3822e211b6a4de7ab8b5988a6e6406496
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 00:46:56 +0300

    [Create] Created get by id product query

---
+++ ERPServer.Application/Features/Products/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Products.GetProductById;
+
+public sealed record GetProductByIdQuery(Guid Id) : IRequest<Result<Product>>;
---
+++ ERPServer.Application/Features/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Products.GetProductById;
+
+internal sealed class GetProductByIdQueryHandler(
+    IProductRepository productRepository) : IRequestHandler<GetProductByIdQuery, Result<Product>>
+{
+    public async Task<Result<Product>> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        Product? product = await productRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (product is null)
+        {
+            return Result<Product>.Failure("Ürün bulunamadı");
+        }
+
+        return product;
+    }
+}
