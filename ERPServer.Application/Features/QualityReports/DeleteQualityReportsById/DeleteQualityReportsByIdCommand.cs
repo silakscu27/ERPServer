5433f64be19e88259124ad916081fd9a2d697dbd
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 16:12:58 +0300

    [Create] Created delete by id quality reports commands

---
+++ ERPServer.Application/Features/QualityReports/DeleteQualityReportsById/DeleteQualityReportsByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityReports.DeleteQualityReportsById;
+
+public sealed record DeleteQualityReportsByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/QualityReports/DeleteQualityReportsById/DeleteQualityReportsByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityReports.DeleteQualityReportsById;
+
+internal sealed class DeleteQualityReportsByIdCommandHandler(
+    IQualityReportRepository qualityReportRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteQualityReportsByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteQualityReportsByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Check if the quality report exists
+        var qualityReport = await qualityReportRepository.GetByExpressionAsync(q => q.Id == request.Id, cancellationToken);
+
+        if (qualityReport is null)
+        {
+            return Result<string>.Failure("Kalite raporu bulunamadı");
+        }
+
+        // Delete the quality report
+        qualityReportRepository.Delete(qualityReport);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result<string>.Succeed("Kalite raporu başarıyla silindi");
+    }
+}
