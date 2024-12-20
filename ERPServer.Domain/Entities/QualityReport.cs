bfc7073513e04b6e24c863888bb476accbdc7be8
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 13:51:19 +0300

    [Create] Created Quality Report entity

---
+++ ERPServer.Domain/Entities/QualityReport.cs
@@ -0,0 +1,11 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class QualityReport : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public string? Description { get; set; }
+}
