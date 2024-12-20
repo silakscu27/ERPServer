b0f73e0c1e5f80e7168ad8ac32d7749fa5787adb
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 15:37:32 +0300

    [Create] Created quality report interface/repository

---
+++ ERPServer.Infrastructure/Repositories/QualityReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class QualityReportRepository : Repository<QualityReport, ApplicationDbContext>, IQualityReportRepository
+{
+    public QualityReportRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
