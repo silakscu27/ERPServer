565915c809c548051d8decf2314c1b75c314819b
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 16:13:23 +0300

    [Create] Created get all quality reports queries

---
+++ ERPServer.Application/Features/QualityReports/GetAllQualityReports/GetAllQualityReportQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityReports.GetAllQualityReports;
+
+public sealed record GetAllQualityReportQuery() : IRequest<Result<List<QualityReport>>>;
---
+++ ERPServer.Application/Features/QualityReports/GetAllQualityReports/GetAllQualityReportQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityReports.GetAllQualityReports;
+
+internal sealed class GetAllQualityReportQueryHandler(
+    IQualityReportRepository qualityReportRepository) : IRequestHandler<GetAllQualityReportQuery, Result<List<QualityReport>>>
+{
+    public async Task<Result<List<QualityReport>>> Handle(GetAllQualityReportQuery request, CancellationToken cancellationToken)
+    {
+        // Fetch all quality reports and order them by CreatedDate
+        List<QualityReport> qualityReports = await qualityReportRepository.GetAll()
+            .OrderBy(q => q.CreatedDate)
+            .ToListAsync(cancellationToken);
+
+        return Result<List<QualityReport>>.Succeed(qualityReports);
+    }
+}
