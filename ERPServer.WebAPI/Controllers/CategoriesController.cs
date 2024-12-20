984982f0802a295a2a3d2fe63b3292f4ceebf8c7
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 13:50:16 +0300

    [Create] Created Category Controller

---
+++ ERPServer.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.Categories.CreateCategory;
+using ERPServer.Application.Features.Categories.DeleteCategoryById;
+using ERPServer.Application.Features.Categories.GetAllCategory;
+using ERPServer.Application.Features.Categories.UpdateCategory;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class CategoriesController : ApiController
+{
+    public CategoriesController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllCategoryQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteCategoryByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
