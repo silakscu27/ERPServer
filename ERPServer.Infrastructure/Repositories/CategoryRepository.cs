dd47006cf1f0081898b6f41f6eac4cab83334a00
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 13:49:56 +0300

    [Create] Created Category and Stock repository

---
+++ ERPServer.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class CategoryRepository : Repository<Category, ApplicationDbContext>, ICategoryRepository
+{
+    public CategoryRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
---
+++ ERPServer.Infrastructure/Repositories/StockRepository.cs
@@ -0,0 +1,20 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+internal sealed class StockRepository : Repository<Stock, ApplicationDbContext>, IStockRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    public StockRepository(ApplicationDbContext context) : base(context)
+    {
+        _dbContext = context;
+    }
+
+    public async Task<Stock?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<Stock>()
+                             .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+    }
+}
