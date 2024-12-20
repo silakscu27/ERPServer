b7e3b2750ceb58f4c4475e1580f8df6e49d0cf23
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 16:43:20 +0300

    [Create] Created Quality Reports Controller

---
+++ ERPServer.WebAPI/Controllers/QualityReportsController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.QualityReports.CreateQualityReports;
+using ERPServer.Application.Features.QualityReports.DeleteQualityReportsById;
+using ERPServer.Application.Features.QualityReports.GetAllQualityReports;
+using ERPServer.Application.Features.QualityReports.UpdateQualityReports;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class QualityReportsController : ApiController
+{
+    public QualityReportsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllQualityReportQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateQualityReportCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteQualityReportsByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateQualityReportCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
