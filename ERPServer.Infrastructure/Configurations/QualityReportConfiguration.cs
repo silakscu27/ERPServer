4969842a1641fbf3e4218f746be3eee0cab83280
Sila Kuscu <silaksc27@gmail.com>
2024-12-20 16:14:09 +0300

    [Create] Created quality report configuration

---
+++ ERPServer.Infrastructure/Configurations/QualityReportConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations
+{
+    public sealed class QualityReportConfiguration : IEntityTypeConfiguration<QualityReport>
+    {
+        public void Configure(EntityTypeBuilder<QualityReport> builder)
+        {
+            builder.ToTable("QualityReports"); 
+
+            builder.HasKey(q => q.Id); 
+
+            builder.Property(q => q.Name)
+                .IsRequired() 
+                .HasMaxLength(100); 
+
+            builder.Property(q => q.CreatedDate)
+                .IsRequired() 
+                .HasColumnType("datetime"); 
+
+            builder.Property(q => q.Description)
+                .HasMaxLength(500); 
+        }
+    }
+}
