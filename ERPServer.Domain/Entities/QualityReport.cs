b445512d37087447c78abdcbee35053812de3d25
Sila Kuscu <silaksc27@gmail.com>
2024-12-22 00:06:52 +0300

    [Add] Added relationship with QualityCheck

--- ERPServer.Domain/Entities/QualityReport.cs
+++ ERPServer.Domain/Entities/QualityReport.cs
@@ -8,4 +8,6 @@ public sealed class QualityReport : Entity
     public string Name { get; set; } = string.Empty;
     public DateTime CreatedDate { get; set; }
     public string? Description { get; set; }
+
+    public ICollection<QualityCheck> QualityChecks { get; set; } = new HashSet<QualityCheck>();
 }
