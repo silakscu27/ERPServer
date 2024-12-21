31013f800ffc28f935cb61b95586156a18d358fe
Sila Kuscu <silaksc27@gmail.com>
2024-12-21 23:50:38 +0300

    [Create] Created QualityChecks Repository

---
+++ ERPServer.Infrastructure/Repositories/QualityChecksRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class QualityChecksRepository : Repository<QualityChecks, ApplicationDbContext>, IQualityChecksRepository
+{
+    public QualityChecksRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
