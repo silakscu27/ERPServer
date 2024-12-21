e8db01b582d86f28213cb7536e20774862d07d03
Sila Kuscu <silaksc27@gmail.com>
2024-12-22 00:05:37 +0300

    [Create] Created QualityCheck entity

---
+++ ERPServer.Domain/Entities/QualityChecks.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class QualityCheck : Entity
+{
+    public new Guid Id { get; set; }
+    public DateTime CheckDate { get; set; }
+    public Guid ReportId { get; set; }
+
+    public string Checkpoint { get; set; } = string.Empty;
+    public string Result { get; set; } = string.Empty;
+
+    public QualityReport QualityReport { get; set; } = null!;
+}
