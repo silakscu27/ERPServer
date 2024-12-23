5c80010b8cb11086c35c54dd8b7b951bb00f1152
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 16:49:54 +0300

    [Create] Created QualityChecks get all queries

---
+++ ERPServer.Application/Features/QualityChecks/GetAllQualityCheck/GetAllQualityCheckQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityChecks.GetAllQualityCheck;
+
+public sealed record GetAllQualityCheckQuery() : IRequest<Result<List<QualityCheck>>>;
---
+++ ERPServer.Application/Features/QualityChecks/GetAllQualityCheck/GetAllQualityCheckQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityChecks.GetAllQualityCheck;
+
+internal sealed class GetAllQualityCheckQueryHandler(
+    IQualityChecksRepository qualityCheckRepository) : IRequestHandler<GetAllQualityCheckQuery, Result<List<QualityCheck>>>
+{
+    public async Task<Result<List<QualityCheck>>> Handle(GetAllQualityCheckQuery request, CancellationToken cancellationToken)
+    {
+        // Fetch all QualityCheck entities and order them by CheckDate
+        List<QualityCheck> qualityChecks = await qualityCheckRepository.GetAll()
+            .OrderBy(q => q.CheckDate)
+            .ToListAsync(cancellationToken);
+     
+        return qualityChecks;
+    }
+}
