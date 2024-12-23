14ba6fe57fc2ddb64f4beda413ed04f6b1587d0f
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 18:41:49 +0300

    [Create] Created Equipment entity

---
+++ ERPServer.Domain/Entities/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Equipment : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string SerialNumber { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
