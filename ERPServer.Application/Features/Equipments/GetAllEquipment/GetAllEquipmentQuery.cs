92c6f81e1fbde04dffb1ca2da267ff88f75b47c8
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 19:17:17 +0300

    [Create] Created Equipment get all query

---
+++ ERPServer.Application/Features/Equipments/GetAllEquipment/GetAllEquipmentQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Equipments.GetAllEquipment;
+
+public sealed record GetAllEquipmentQuery() : IRequest<Result<List<Equipment>>>;
