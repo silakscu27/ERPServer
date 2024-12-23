a0bf05dc9c44e5f33488b4529b532003fee8318c
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 16:49:25 +0300

    [Create] Created QualityChecks delete by id commands

---
+++ ERPServer.Application/Features/QualityChecks/DeleteQualityCheckById/DeleteQualityCheckByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityChecks.DeleteQualityCheckById;
+
+public sealed record DeleteQualityCheckByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/QualityChecks/DeleteQualityCheckById/DeleteQualityCheckByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.QualityChecks.DeleteQualityCheckById;
+
+internal sealed class DeleteQualityCheckByIdCommandHandler(
+    IQualityChecksRepository qualityCheckRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteQualityCheckByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteQualityCheckByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve the QualityCheck entity by its ID
+        var qualityCheck = await qualityCheckRepository.GetByExpressionAsync(q => q.Id == request.Id, cancellationToken);
+
+        if (qualityCheck is null)
+        {
+            return Result<string>.Failure("Kalite kontrol kaydı bulunamadı");
+        }
+
+        // Delete the QualityCheck entity
+        qualityCheckRepository.Delete(qualityCheck);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Kalite kontrol kaydı başarıyla silindi";
+    }
+}
