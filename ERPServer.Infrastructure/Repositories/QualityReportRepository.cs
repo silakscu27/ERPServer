2c27f75b4a9c63a3075ea530deaa74c1b6af3acb
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 18:43:51 +0300

    [Add] getbyid method defined

--- ERPServer.Infrastructure/Repositories/QualityReportRepository.cs
+++ ERPServer.Infrastructure/Repositories/QualityReportRepository.cs
@@ -2,11 +2,19 @@
 using ERPServer.Domain.Repositories;
 using ERPServer.Infrastructure.Context;
 using GenericRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace ERPServer.Infrastructure.Repositories;
 internal sealed class QualityReportRepository : Repository<QualityReport, ApplicationDbContext>, IQualityReportRepository
 {
+    private readonly ApplicationDbContext _dbContext;
     public QualityReportRepository(ApplicationDbContext context) : base(context)
     {
+        _dbContext = context;
+    }
+    public async Task<QualityReport?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<QualityReport>()
+                             .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
     }
 }
