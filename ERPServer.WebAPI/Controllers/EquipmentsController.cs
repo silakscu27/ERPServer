27f98d0fd162859dba03b5884a5e8814ca99ca6c
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 19:22:20 +0300

    [Create] Created Equipments Controller

---
+++ ERPServer.WebAPI/Controllers/EquipmentsController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.Equipments.CreateEquipment;
+using ERPServer.Application.Features.Equipments.DeleteEquipmentById;
+using ERPServer.Application.Features.Equipments.GetAllEquipment;
+using ERPServer.Application.Features.Equipments.UpdateEquipment;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class EquipmentsController : ApiController
+{
+    public EquipmentsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllEquipmentQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateEquipmentCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteEquipmentByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateEquipmentCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
