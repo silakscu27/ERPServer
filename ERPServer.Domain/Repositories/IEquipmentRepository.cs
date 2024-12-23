2835e149b2f3dd00cfc1dfad303020e7a232d716
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 18:59:28 +0300

    [Add] Added Find method

--- ERPServer.Domain/Repositories/IEquipmentRepository.cs
+++ ERPServer.Domain/Repositories/IEquipmentRepository.cs
@@ -1,7 +1,9 @@
 ﻿using ERPServer.Domain.Entities;
 using GenericRepository;
+using System.Linq.Expressions;
 
 namespace ERPServer.Domain.Repositories;
 public interface IEquipmentRepository : IRepository<Equipment>
 {
+    Task<Equipment?> FindAsync(Expression<Func<Equipment, bool>> predicate, CancellationToken cancellationToken);
 }
--- ERPServer.Infrastructure/Repositories/EquipmentRepository.cs
+++ ERPServer.Infrastructure/Repositories/EquipmentRepository.cs
@@ -2,11 +2,20 @@
 using ERPServer.Domain.Repositories;
 using ERPServer.Infrastructure.Context;
 using GenericRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ERPServer.Infrastructure.Repositories;
 internal sealed class EquipmentRepository : Repository<Equipment, ApplicationDbContext>, IEquipmentRepository
 {
+    private readonly ApplicationDbContext _dbContext;
     public EquipmentRepository(ApplicationDbContext context) : base(context)
     {
+        _dbContext = context;
+    }
+
+    public async Task<Equipment?> FindAsync(Expression<Func<Equipment, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<Equipment>().FirstOrDefaultAsync(predicate, cancellationToken);
     }
 }
