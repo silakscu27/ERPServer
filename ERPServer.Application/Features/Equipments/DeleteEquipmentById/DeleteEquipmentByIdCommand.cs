83b6e5f4b76c3eb9da437f7bf390267b614942f1
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 19:02:59 +0300

    [Create] Created Equipment delete by id command

---
+++ ERPServer.Application/Features/Equipments/DeleteEquipmentById/DeleteEquipmentByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Equipments.DeleteEquipmentById;
+
+public sealed record DeleteEquipmentByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Equipments/DeleteEquipmentById/DeleteEquipmentByIdCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Equipments.DeleteEquipmentById;
+
+internal sealed class DeleteEquipmentByIdCommandHandler(
+    IEquipmentRepository equipmentRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteEquipmentByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteEquipmentByIdCommand request, CancellationToken cancellationToken)
+    {
+        Equipment equipment = await equipmentRepository.GetByExpressionAsync(e => e.Id == request.Id, cancellationToken);
+
+        if (equipment is null)
+        {
+            return Result<string>.Failure("Ekipman bulunamadı");
+        }
+
+        equipmentRepository.Delete(equipment);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Ekipman başarıyla silindi";
+    }
+}
