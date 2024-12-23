76de4d914b6a53f97f6fb95e5d79deff907c2412
Sila Kuscu <silaksc27@gmail.com>
2024-12-23 18:29:07 +0300

    [Create] Created Equipment Repository

---
+++ ERPServer.Infrastructure/Repositories/EquipmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class EquipmentRepository : Repository<Equipment, ApplicationDbContext>, IEquipmentRepository
+{
+    public EquipmentRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
