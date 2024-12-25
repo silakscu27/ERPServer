0a41760c2a8ed5c97f72e81ed1c3cb9670da5a73
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 15:55:11 +0300

    [Create] Created MaintenanceTask get all queries

---
+++ ERPServer.Application/Features/MaintenanceTasks/GetAllMaintenanceTask/GetAllMaintenanceTaskQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.MaintenanceTasks.GetAllMaintenanceTask;
+
+public sealed record GetAllMaintenanceTaskQuery() : IRequest<Result<List<MaintenanceTask>>>;
---
+++ ERPServer.Application/Features/MaintenanceTasks/GetAllMaintenanceTask/GetAllMaintenanceTaskQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.MaintenanceTasks.GetAllMaintenanceTask;
+
+internal sealed class GetAllMaintenanceTaskQueryHandler(
+    IMaintenanceTaskRepository maintenanceTaskRepository) : IRequestHandler<GetAllMaintenanceTaskQuery, Result<List<MaintenanceTask>>>
+{
+    public async Task<Result<List<MaintenanceTask>>> Handle(GetAllMaintenanceTaskQuery request, CancellationToken cancellationToken)
+    {
+        List<MaintenanceTask> maintenanceTasks = await maintenanceTaskRepository.GetAll()
+            .OrderBy(mt => mt.ScheduledDate)
+            .ToListAsync(cancellationToken);
+
+        return maintenanceTasks;
+    }
+}
