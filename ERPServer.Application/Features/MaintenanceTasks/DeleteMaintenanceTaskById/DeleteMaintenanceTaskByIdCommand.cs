7187375205028bcab710454f3e786f3149d88818
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 15:30:51 +0300

    [Create] Created MaintenanceTasks delete by id command

---
+++ ERPServer.Application/Features/MaintenanceTasks/DeleteMaintenanceTaskById/DeleteMaintenanceTaskByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.MaintenanceTasks.DeleteMaintenanceTaskById;
+
+public sealed record DeleteMaintenanceTaskByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/MaintenanceTasks/DeleteMaintenanceTaskById/DeleteMaintenanceTaskByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.MaintenanceTasks.DeleteMaintenanceTaskById;
+
+internal sealed class DeleteMaintenanceTaskByIdCommandHandler(
+    IMaintenanceTaskRepository maintenanceTaskRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteMaintenanceTaskByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteMaintenanceTaskByIdCommand request, CancellationToken cancellationToken)
+    {
+        MaintenanceTask? maintenanceTask = await maintenanceTaskRepository.GetByExpressionAsync(
+            mt => mt.Id == request.Id, cancellationToken);
+
+        if (maintenanceTask is null)
+        {
+            return Result<string>.Failure("Bakım görevi bulunamadı");
+        }
+
+        maintenanceTaskRepository.Delete(maintenanceTask);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Bakım görevi başarıyla silindi";
+    }
+}
