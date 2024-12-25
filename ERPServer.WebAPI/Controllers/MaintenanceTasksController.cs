e8f00f137441f37edb20cdabcf1def48122a180d
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 16:21:05 +0300

    [Create] Created MaintenanceTask Controller

---
+++ ERPServer.WebAPI/Controllers/MaintenanceTasksController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.MaintenanceTasks.CreateMaintenanceTask;
+using ERPServer.Application.Features.MaintenanceTasks.DeleteMaintenanceTaskById;
+using ERPServer.Application.Features.MaintenanceTasks.GetAllMaintenanceTask;
+using ERPServer.Application.Features.MaintenanceTasks.UpdateMaintenanceTask;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class MaintenanceTasksController : ApiController
+{
+    public MaintenanceTasksController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllMaintenanceTaskQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateMaintenanceTaskCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteMaintenanceTaskByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateMaintenanceTaskCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
