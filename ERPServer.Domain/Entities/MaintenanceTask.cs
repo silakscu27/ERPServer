1ae2bc905fcd0051e09f9faccaeef2790c7ae628
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 14:28:03 +0300

    [Create] Created MaintenanceTask entity

---
+++ ERPServer.Domain/Entities/MaintenanceTask.cs
@@ -0,0 +1,14 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class MaintenanceTask : Entity
+{
+    public new Guid Id { get; set; }
+    public Guid EquipmentId { get; set; }
+    public string TaskName { get; set; } = string.Empty;
+    public DateTime ScheduledDate { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public Equipment Equipment { get; set; } = null!;
+
+}
