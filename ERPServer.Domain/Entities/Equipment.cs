3398feef53e2dbc8ee4ee3b48235e2907576578a
Sila Kuscu <silaksc27@gmail.com>
2024-12-25 14:27:31 +0300

    [Add] Added relationship between MaintenanceTask and Equipment

--- ERPServer.Domain/Entities/Equipment.cs
+++ ERPServer.Domain/Entities/Equipment.cs
@@ -8,4 +8,6 @@ public sealed class Equipment : Entity
     public string Name { get; set; } = string.Empty;
     public string SerialNumber { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
+
+    public ICollection<MaintenanceTask> MaintenanceTasks { get; set; } = new List<MaintenanceTask>();
 }
