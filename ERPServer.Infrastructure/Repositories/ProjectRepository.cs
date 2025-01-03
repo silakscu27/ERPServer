3f27ca793d0059c1d209818cf17d4b13a7479797
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 10:10:48 +0300

    [Create] Created Project interface/repo

---
+++ ERPServer.Infrastructure/Repositories/ProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+internal sealed class ProjectRepository : Repository<Project, ApplicationDbContext>, IProjectRepository
+{
+    public ProjectRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
