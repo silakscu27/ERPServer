6a2c62820df8262a0487ee3aaabb6fa3e922ff76
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 11:16:01 +0300

    [Create] Created Project Controller

---
+++ ERPServer.WebAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,47 @@
+﻿using ERPServer.Application.Features.Products.GetAllProduct;
+using ERPServer.Application.Features.Projects.CreateProject;
+using ERPServer.Application.Features.Projects.DeleteProjectById;
+using ERPServer.Application.Features.Projects.GetProjectById;
+using ERPServer.Application.Features.Projects.UpdateProject;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class ProjectsController : ApiController
+{
+    public ProjectsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new GetProjectByIdQuery(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateProjectCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteProjectByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateProjectCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
