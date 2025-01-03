17354233c18443dc2d30da92fc450cdfef34eb8a
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 10:06:20 +0300

    [Create] Created Project entity

---
+++ ERPServer.Domain/Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Project : Entity
+{
+    public new Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; } 
+    public string Status { get; set; } = string.Empty; 
+
+}
