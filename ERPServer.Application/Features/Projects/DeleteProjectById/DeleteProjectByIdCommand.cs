cc13c84c7e852aa0eee0a0794b60e35075fc37bf
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 10:35:34 +0300

    [Create] Cretated Project Delete By Id commands

---
+++ ERPServer.Application/Features/Projects/DeleteProjectById/DeleteProjectByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Projects.DeleteProjectById;
+
+public sealed record DeleteProjectByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Projects/DeleteProjectById/DeleteProjectByIdCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Projects.DeleteProjectById;
+
+internal sealed class DeleteProjectByIdCommandHandler(
+    IProjectRepository projectRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteProjectByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteProjectByIdCommand request, CancellationToken cancellationToken)
+    {
+        Project project = await projectRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (project is null)
+        {
+            return Result<string>.Failure("Proje bulunamadı");
+        }
+
+        projectRepository.Delete(project);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Proje başarıyla silindi";
+    }
+}
