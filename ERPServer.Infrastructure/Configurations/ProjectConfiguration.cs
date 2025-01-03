16d204e98f37b479e090c857133a16ac80e96250
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 10:13:23 +0300

    [Create] Created Project Configuration

---
+++ ERPServer.Infrastructure/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations;
+
+public sealed class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable("Projects"); 
+
+        builder.HasKey(p => p.Id); 
+
+        builder.Property(p => p.Name)
+            .IsRequired() 
+            .HasMaxLength(200); 
+
+        builder.Property(p => p.StartDate)
+            .IsRequired(); 
+
+        builder.Property(p => p.EndDate)
+            .IsRequired(false); 
+
+        builder.Property(p => p.Status)
+            .IsRequired() 
+            .HasMaxLength(50); 
+
+    }
+}
