2742e5a2716d09cc8afbf53f95be1e789be6d7c3
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 11:31:01 +0300

    [Create] Created Milestone entity

---
+++ ERPServer.Domain/Entities/Milestone.cs
@@ -0,0 +1,13 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Milestone : Entity
+{
+    public new Guid Id { get; set; } 
+    public Guid ProjectId { get; set; } 
+    public string MilestoneName { get; set; } = string.Empty; 
+    public int EstimatedTime { get; set; }
+
+    public Project Project { get; set; } = default!; 
+}
