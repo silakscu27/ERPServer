7d80a46742b100905a0c6f8582ce5237c9a0e8ae
Sila Kuscu <silaksc27@gmail.com>
2025-01-03 10:35:51 +0300

    [Create] Cretated Project Get By Id queries

---
+++ ERPServer.Application/Features/Projects/GetProjectById/GetProjectByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Projects.GetProjectById;
+
+public sealed record GetProjectByIdQuery(Guid Id) : IRequest<Result<Project>>;
---
+++ ERPServer.Application/Features/Projects/GetProjectById/GetProjectByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Projects.GetProjectById;
+
+internal sealed class GetProjectByIdQueryHandler(
+    IProjectRepository projectRepository) : IRequestHandler<GetProjectByIdQuery, Result<Project>>
+{
+    public async Task<Result<Project>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+    {
+        Project project = await projectRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (project is null)
+        {
+            return Result<Project>.Failure("Proje bulunamadı");
+        }
+
+        return project;
+    }
+}
