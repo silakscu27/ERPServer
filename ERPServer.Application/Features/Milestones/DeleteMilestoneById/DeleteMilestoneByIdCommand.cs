a891b6c6bf07442743fdd55ee545472f21a9b8c1
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 20:17:48 +0300

    [Create] Created milestone Delete by id commands

---
+++ ERPServer.Application/Features/Milestones/DeleteMilestoneById/DeleteMilestoneByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Milestones.DeleteMilestoneById;
+
+public sealed record DeleteMilestoneByIdCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer.Application/Features/Milestones/DeleteMilestoneById/DeleteMilestoneByIdCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Milestones.DeleteMilestoneById;
+
+internal sealed class DeleteMilestoneByIdCommandHandler(
+    IMilestoneRepository milestoneRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteMilestoneByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteMilestoneByIdCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve the milestone by its ID
+        Milestone? milestone = await milestoneRepository.GetByExpressionAsync(m => m.Id == request.Id, cancellationToken);
+
+        if (milestone is null)
+        {
+            return Result<string>.Failure("Aşama bulunamadı");
+        }
+
+        // Delete the milestone
+        milestoneRepository.Delete(milestone);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Aşama başarıyla silindi";
+    }
+}
