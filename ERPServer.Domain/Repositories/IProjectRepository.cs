95d12114045f18fd72db9ae3d378124434b4a03f
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:14:07 +0300

    [Add] Added GetById method

--- ERPServer.Domain/Repositories/IProjectRepository.cs
+++ ERPServer.Domain/Repositories/IProjectRepository.cs
@@ -4,4 +4,5 @@ using GenericRepository;
 namespace ERPServer.Domain.Repositories;
 public interface IProjectRepository : IRepository<Project>
 {
+    Task<Project?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 }
--- ERPServer.Infrastructure/Repositories/ProjectRepository.cs
+++ ERPServer.Infrastructure/Repositories/ProjectRepository.cs
@@ -2,11 +2,19 @@
 using ERPServer.Domain.Repositories;
 using ERPServer.Infrastructure.Context;
 using GenericRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace ERPServer.Infrastructure.Repositories;
 internal sealed class ProjectRepository : Repository<Project, ApplicationDbContext>, IProjectRepository
 {
+    private readonly ApplicationDbContext _dbContext;
     public ProjectRepository(ApplicationDbContext context) : base(context)
     {
+        _dbContext = context;
+    }
+    public async Task<Project?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Set<Project>()
+            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
     }
 }
