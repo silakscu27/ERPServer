5ce91db1332e34f77fe9c512354884efcd4977d1
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:14:29 +0300

    [Create] Created Milestone Controller

---
+++ ERPServer.WebAPI/Controllers/MilestonesController.cs
@@ -0,0 +1,45 @@
+﻿using ERPServer.Application.Features.Milestones.CreateMilestone;
+using ERPServer.Application.Features.Milestones.DeleteMilestoneById;
+using ERPServer.Application.Features.Milestones.GetAllMilestone;
+using ERPServer.Application.Features.Milestones.UpdateMilestone;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers;
+
+public sealed class MilestonesController : ApiController
+{
+    public MilestonesController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(GetAllMilestoneQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateMilestoneCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteMilestoneByIdCommand(id);
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateMilestoneCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+}
