feed6c1ccd86c2c0c086965a29bdbe1e58dae91d
Sila Kuscu <silaksc27@gmail.com>
2025-01-06 23:11:09 +0300

    [Create] Created Milestone get all query

---
+++ ERPServer.Application/Features/Milestones/GetAllMilestone/GetAllMilestoneQuery.cs
@@ -0,0 +1,7 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Milestones.GetAllMilestone;
+
+public sealed record GetAllMilestoneQuery() : IRequest<Result<List<Milestone>>>;
---
+++ ERPServer.Application/Features/Milestones/GetAllMilestone/GetAllMilestoneQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Milestones.GetAllMilestone;
+
+internal sealed class GetAllMilestoneQueryHandler(
+    IMilestoneRepository milestoneRepository) : IRequestHandler<GetAllMilestoneQuery, Result<List<Milestone>>>
+{
+    public async Task<Result<List<Milestone>>> Handle(GetAllMilestoneQuery request, CancellationToken cancellationToken)
+    {
+        // Fetch all milestones and order them by name
+        List<Milestone> milestones = await milestoneRepository.GetAll()
+            .OrderBy(m => m.MilestoneName)
+            .ToListAsync(cancellationToken);
+
+        return milestones;
+    }
+}
